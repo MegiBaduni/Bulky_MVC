86713229c8d53e0ca8bb10c690efff3c5ad783b9
Meg <153324621+MegiBaduni@users.noreply.github.com>
2024-12-05 17:58:50 +0100

    Update HomeController.cs

--- BulkyWeb/Controllers/HomeController.cs
+++ BulkyWeb/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ namespace BulkyWeb.Controllers
 {
     public class HomeController : Controller
     {
+
+    
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
